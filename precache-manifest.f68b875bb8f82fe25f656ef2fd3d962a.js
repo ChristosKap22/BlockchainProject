482fbd5b1f2d9117bc1e702f0457ba5fdaf29929
ChristosKap22 <xrkp20@hotmail.com>
2022-07-07 20:30:17 +0300

    Updates

---
+++ precache-manifest.f68b875bb8f82fe25f656ef2fd3d962a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "42626f73b68c2b207599",
+    "url": "/Blockchain2022Project/static/css/main.f7cbacd6.chunk.css"
+  },
+  {
+    "revision": "42626f73b68c2b207599",
+    "url": "/Blockchain2022Project/static/js/main.42626f73.chunk.js"
+  },
+  {
+    "revision": "5995e8befb12c935891e",
+    "url": "/Blockchain2022Project/static/css/1.0a2aeee1.chunk.css"
+  },
+  {
+    "revision": "5995e8befb12c935891e",
+    "url": "/Blockchain2022Project/static/js/1.5995e8be.chunk.js"
+  },
+  {
+    "revision": "b4f27d9ed5cee7d2eaa9",
+    "url": "/Blockchain2022Project/static/js/runtime~main.b4f27d9e.js"
+  },
+  {
+    "revision": "c1b6b40e58523fa4f89d2f0b026da091",
+    "url": "/Blockchain2022Project/static/media/car.c1b6b40e.jpg"
+  },
+  {
+    "revision": "555238221336435f5b4d0719cca9f8fe",
+    "url": "/Blockchain2022Project/static/media/phone.55523822.jpg"
+  },
+  {
+    "revision": "211b9607c96d9481d834b868b210ac20",
+    "url": "/Blockchain2022Project/index.html"
+  }
+];
